53168a2e6694fcd4e5f7131860e3c7b968d2ff80
dead programmer <46463357+dead-programmer@users.noreply.github.com>
2019-04-12 13:36:28 -0500

    Update lobby.py

--- lobby.py
+++ lobby.py
@@ -83,6 +83,7 @@ def lobby_level(player):
         "x": 0,
         "y": 9
     }
+    gloria_talked = False
 
     while 1:
         keys = {"w": 0, "a": 0, "s": 0, "d": 0}
@@ -91,7 +92,7 @@ def lobby_level(player):
         display.blit(player, [playerpos["xpix"], playerpos["ypix"]])
 
         # Gloria conversation
-        if (playerpos["x"] == 6 and playerpos["y"] == 6) or (playerpos["x"] == 6 and playerpos["y"] == 3) or (playerpos["x"] == 7 and playerpos["y"] == 6) or (playerpos["x"] == 7 and playerpos["y"] == 4):
+        if (playerpos["x"] == 6 and playerpos["y"] in [6, 3]) or (playerpos["x"] == 7 and playerpos["y"] in [6, 4]):
             if gloria_speak:
                 by_letter = create_dialogue_box(display, ["Hi! My name is Gloria. Welcome to",
                                                           "Adtran! We've got a lot for you to get",
@@ -104,8 +105,14 @@ def lobby_level(player):
         else:
             gloria_speak = False
             by_letter = [0, 0]
+
+
         if playerpos["x"] in [14, 15] and playerpos["y"] == 2:
-            create_popup(display, "Press Spacebar to Enter Elevator")
+            if gloria_talked:
+                create_popup(display, "Press Spacebar to Enter Elevator")
+            else:
+                create_popup(display, "Adtran badge needed")
+
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -120,10 +127,11 @@ def lobby_level(player):
                     keys["s"] = 1
                 elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                     keys["d"] = 1
-                elif event.key == pygame.K_SPACE and playerpos["x"] in [14, 15] and playerpos["y"] == 2:
+                elif event.key == pygame.K_SPACE and playerpos["x"] in [14, 15] and playerpos["y"] == 2 and gloria_talked is True:
                     into_the_elevator = True
-                elif event.key == pygame.K_r and playerpos["x"] == 6 and playerpos["y"] == 6:
+                elif event.key == pygame.K_r and (playerpos["x"] == 6 and playerpos["y"] == 6) or (playerpos["x"] == 6 and playerpos["y"] == 3) or (playerpos["x"] == 7 and playerpos["y"] == 6) or (playerpos["x"] == 7 and playerpos["y"] == 4):
                     gloria_speak = True
+                    gloria_talked = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_w or event.key == pygame.K_UP:
                     keys["w"] = 0
